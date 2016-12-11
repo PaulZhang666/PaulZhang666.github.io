29311c5b86d9f10a0f4fef7fb944ea86068fd152
PaulZhang <henry14ab@yahoo.com>
2016-12-11 15:09:49 -0500

    Edition 1.0!

--- footprints/footprints.js
+++ footprints/footprints.js
@@ -12,16 +12,119 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('footprintCtrl', ['$scope', footprintCtrl]);
 
 function footprintCtrl($scope){
-    var eleOne = document.getElementsByClassName('header-background')[0];
-    var eleTwo = document.getElementsByClassName('header-background-2')[0];
+    var background_img = document.getElementsByClassName('header-background_img')[0];
+    var background_opa = document.getElementsByClassName('header-background_opa')[0];
+    var title = document.getElementsByClassName('title')[0];
+    var subtitle = document.getElementsByClassName('subtitle')[0];
+    var content = document.getElementsByClassName('content')[0];
 
     //eleOne.classList.add('sec_0');
 
+    $scope.showStartButton = true;
+
     $scope.showUp = function() {
-        eleOne.classList.add('up');
-        eleTwo.classList.add('up');
+        background_img.classList.add('up');
+        background_opa.classList.add('up');
+        title.classList.add('up');
+        subtitle.classList.add('up');
+        content.classList.add('up');
+        $scope.showStartButton = false;
     };
 
-    $scope.a = "aaa";
+    window.onscroll = function(){
+        $scope.showUp();
+        $scope.$apply();
+    };
+
+    var option = {
+        title: {
+            text: 'Provinces I Have Been Since Birth',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data:[]
+        },
+        visualMap: {
+            min: 0,
+            max: 23,
+            left: '10%',
+            top: '350px',
+            text: ['my age = 23','my age = 0'],           // 文本，默认为数值文本
+            calculable: true
+        },
+        toolbox: {
+            show: false,
+            orient: 'vertical',
+            left: 'right',
+            top: 'center',
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        series: [
+            {
+                name: 'First visit age',
+                type: 'map',
+                mapType: 'china',
+                roam: false,
+                label: {
+                    normal: {
+                        show: true
+                    },
+                    emphasis: {
+                        show: true
+                    }
+                },
+                data:[
+                    {name: 'Beijing',value: 0 },
+                    {name: 'Tianjin',value: 21 },
+                    {name: 'Shanghai',value: 22 },
+                    {name: 'Chongqing',value: "" },
+                    {name: 'Hebei',value: 6 },
+                    {name: 'Henan',value: 21 },
+                    {name: 'Yunnan',value: "" },
+                    {name: 'Liaoning',value: "" },
+                    {name: 'Heilongjiang',value: "" },
+                    {name: 'Hunan',value: 22 },
+                    {name: 'Anhui',value: "" },
+                    {name: 'Shandong',value: 22 },
+                    {name: 'Xinjiang',value: 20 },
+                    {name: 'Jiangsu',value: "" },
+                    {name: 'Zhejiang',value: "" },
+                    {name: 'Jiangxi',value: "" },
+                    {name: 'Hubei',value: "" },
+                    {name: 'Guangxi',value: 23 },
+                    {name: 'Gansu',value: 20 },
+                    {name: 'Shanxi',value: "" },
+                    {name: 'Neimeng',value: 1 },
+                    {name: 'Shan_xi',value: 19 },
+                    {name: 'Jilin',value: "" },
+                    {name: 'Fujian',value: "" },
+                    {name: 'Guizhou',value: "" },
+                    {name: 'Guangdong',value: "" },
+                    {name: 'Qinghai',value: 20 },
+                    {name: 'Tibet',value: 20 },
+                    {name: 'Sichuan',value: "" },
+                    {name: 'Ningxia',value: "" },
+                    {name: 'Hainan',value: 7 },
+                    {name: 'Taiwan',value: "" },
+                    {name: 'Hongkong',value: "" },
+                    {name: 'Makau',value: "" }
+                ]
+            }
+        ]
+    };
+
+    var myChart = echarts.init(document.getElementById('chinaChart'));
+
+    myChart.setOption(option);
+
 
 }
--- startPage/startPage.js
+++ startPage/startPage.js
@@ -13,7 +13,7 @@ app.config(['$routeProvider', function($routeProvider) {
 
 app.controller('startPageCtrl', ['$scope', startPageCtrl]);
 
-function startPageCtrl($scope){
+function startPageCtrl($scope) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     //var pRatio = window.devicePixelRatio || 1;
@@ -28,42 +28,39 @@ function startPageCtrl($scope){
     canvas.height = height;
     ctx.globalAlpha = 0.7;
     //transparency
-    $scope.iteration = function(){
+    $scope.iteration = function () {
         ctx.clearRect(0, 0, width, height);
-        point = [{x:0, y:height * 0.7 + floating},
-            {x:0, y:height * 0.7 -floating}];
-        while(point[1].x < width + floating){
+        point = [{x: 0, y: height * 0.7 + floating},
+            {x: 0, y: height * 0.7 - floating}];
+        while (point[1].x < width + floating) {
             $scope.draw(point[0], point[1]);
         }
     };
-    $scope.draw = function(point_0, point_1){
+    $scope.draw = function (point_0, point_1) {
         ctx.beginPath();
         ctx.moveTo(point_0.x, point_0.y);
         ctx.lineTo(point_1.x, point_1.y);
-        var next_x = point_1.x + (Math.random()*2-0.25)*floating;
+        var next_x = point_1.x + (Math.random() * 2 - 0.25) * floating;
         var next_y = $scope.calY(point_1.y);
         ctx.lineTo(next_x, next_y);
         ctx.closePath();
-        r += 1/25 * Math.PI;
+        r += 1 / 25 * Math.PI;
         ctx.fillStyle = $scope.calColor(r);
         ctx.fill();
         point[0] = point[1];
-        point[1] = {x:next_x, y:next_y};
+        point[1] = {x: next_x, y: next_y};
     };
-    $scope.calY = function(y){
-        var next_y = y + (Math.random()*2 - 1.3)*floating;
-        return (next_y > height || next_y < 0)?$scope.calY(y):next_y;
+    $scope.calY = function (y) {
+        var next_y = y + (Math.random() * 2 - 1.3) * floating;
+        return (next_y > height || next_y < 0) ? $scope.calY(y) : next_y;
     };
-    $scope.calColor = function(r){
+    $scope.calColor = function (r) {
         return '#' + ((Math.sin(r) * 107 + 148) << 16 |
-            (Math.sin(r + 2/3 * Math.PI) * 107 + 148) << 8 |
-            (Math.sin(r + 4/3 * Math.PI) * 107 + 148)).toString(16);
+            (Math.sin(r + 2 / 3 * Math.PI) * 107 + 148) << 8 |
+            (Math.sin(r + 4 / 3 * Math.PI) * 107 + 148)).toString(16);
     };
     document.onclick = $scope.iteration;
     document.ontouchstart = $scope.iteration;
     $scope.iteration();
 
-    $scope.nextPage = function(num){
-        PageTransitions.init.nextPage();
-    }
 }
